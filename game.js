a7ac2b92621e0eb449b3e8168c7edea0628f4572
Parav Kaushal <paravkaushal.kv1@gmail.com>
2020-09-16 12:16:20 +0530

    set up files

---
+++ game.js
@@ -0,0 +1,7 @@
+buttonColors = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  console.log(randomNumber);
+}
+nextSequence();
